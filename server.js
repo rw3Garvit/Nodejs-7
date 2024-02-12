9caeadd079ca01e672e617fb6743e3e87b8acfb3
Garvit Dhameliya <rw3.garvit.rd@gmail.com>
2024-02-12 08:05:38 +0530

    req from html

--- server.js
+++ server.js
@@ -2,50 +2,66 @@ require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
-const app =express()
+const path = require('path')
+
+const app = express()
+
 const route = express.Router()
 
 
-
+//body parser
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
-
 route.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message:"get req success",
-        data:[1,2,3]
-    })
-})
 
-route.get('/getuser',(req,res,next)=>{
-    res.status(200).json({
-        message:'user get success'
-    })
+const file = path.join(__dirname,'/index.html')
+
+    res.sendFile(file)
 })
 
 
 route.post('/',(req,res,next)=>{
 
+let {username,password} = req.body
     console.log(req.body)
 
-    let body = req.body
+    res.status(200).json({
+        message:'data add success',
+        data:{username}
+    })
 
+})
+
+
+route.post('/add',(req,res,next)=>{
+
+
+    const body = req.body
+
+    let num1=parseInt(body.num1)
+    let num2=parseInt(body.num2)
+    
+    let sum=num1+num2
 
     res.status(200).json({
-        message:"data added success",
-        data:body
+        message:'sum complate',
+        sum
     })
-  
 
 })
 
 
 
 
+//routes
 app.use(route)
 
-
-http.createServer().listen(process.env.PORT,()=>{
-    console.log("server started");
+//server
+http.createServer(app).listen(process.env.PORT,()=>{
+    console.log('server start')
 })
+
+// app.listen(3001,()=>{
+//     console.log('server started')
+// })
